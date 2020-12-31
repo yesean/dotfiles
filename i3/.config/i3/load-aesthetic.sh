50aeb89b60d8e0a223ab0dda05e921f6ca3506df
Sean Ye <seanye24@gmail.com>
2020-12-31 01:01:55 -0800

    Update i3 aesthetic layout

--- i3/.config/i3/load-aesthetic.sh
+++ i3/.config/i3/load-aesthetic.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-ws="$1"
+if [ -z "$1" ]
+then
+  ws=1
+else
+  ws=$1
+fi
 
 # start on workspace 1
-i3-msg "workspace ${ws}; append_layout ~/.config/i3/workspace-1.json"
+i3-msg "workspace ${ws}; append_layout ~/.config/i3/aesthetic.json"
 (alacritty --hold -t wttr --class wttr -e curl wttr.in/?0 &)
 (alacritty --hold -t neofetch --class neofetch -e neofetch &)
 (alacritty --hold -t unimatrix --class unimatrix -e ~/terminal-rain.sh &)
