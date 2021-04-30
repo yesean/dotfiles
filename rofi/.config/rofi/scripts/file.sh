cdd3955de4ff97871bb488079549b7acd86de842
Sean Ye <seanye24@gmail.com>
2021-04-30 15:42:48 -0700

    ignore venv in rofi search

--- rofi/.config/rofi/scripts/file.sh
+++ rofi/.config/rofi/scripts/file.sh
@@ -5,5 +5,5 @@ if [[ $query != "" ]]; then
   coproc (xdg-open "$query" &> /dev/null)
   exit
 else
-  fd --follow --exclude "{.git,node_modules,games}" . ~
+  fd --follow --exclude "{.git,node_modules,games,venv}" . ~
 fi
