eecfcc7521062e8b721d6168e34e95d3e05d71ab
Sean Ye <seanye24@gmail.com>
2021-05-11 17:28:36 -0700

    Update vscode config

--- vscode/.config/Code/scripts/install-extensions.sh
+++ vscode/.config/Code/scripts/install-extensions.sh
@@ -4,4 +4,4 @@
 # rm -rf ~/.vscode/extensions/
 
 # install extensions
-xargs -pn 1 code --force --install-extension < ~/.dotfiles/vscode/.config/Code/extensions.txt
+xargs -n 1 code --force --install-extension < ~/.dotfiles/vscode/.config/Code/extensions.txt
