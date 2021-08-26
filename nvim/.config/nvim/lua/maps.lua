c04fba23caeee2496aee83b51af138ac44048f95
Sean Ye <yesean@amazon.com>
2021-08-26 13:47:33 -0700

    Update mappings

--- nvim/.config/nvim/lua/maps.lua
+++ nvim/.config/nvim/lua/maps.lua
@@ -1,10 +1,25 @@
 local map = vim.api.nvim_set_keymap
+opts = { noremap = true }
 
+-- map leader to space
 map('n', '<space>', '', {})
 vim.g.mapleader = ' '
 
-opts = { noremap = true }
+-- pane switching
 map('n', '<leader>h', ':wincmd h<cr>', opts)
 map('n', '<leader>j', ':wincmd j<cr>', opts)
 map('n', '<leader>k', ':wincmd k<cr>', opts)
 map('n', '<leader>l', ':wincmd l<cr>', opts)
+
+-- clipboard operations
+-- Copy to clipboard
+map('v', '<leader>y', '"+y', opts)
+map('n', '<leader>y', '"+y', opts)
+map('n', '<leader>yg_', '"+yg_', opts)
+map('n', '<leader>yy', '"+yy', opts)
+
+-- Paste from clipboard
+map('v', '<leader>p', '"+y', opts)
+map('v', '<leader>P', '"+Y', opts)
+map('n', '<leader>p', '"+y', opts)
+map('n', '<leader>P', '"+Y', opts)
