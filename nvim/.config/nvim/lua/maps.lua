d1ee27fdbed3a92374b684306ef7a400e99edeb7
Sean Ye <yesean@amazon.com>
2021-08-31 11:41:58 -0700

    Update mappings

--- nvim/.config/nvim/lua/maps.lua
+++ nvim/.config/nvim/lua/maps.lua
@@ -1,25 +1,53 @@
 local map = vim.api.nvim_set_keymap
-opts = { noremap = true }
+local modemap = function(mode)
+  return function(binding, command, extra)
+    extra = extra or {}
+    local opts = { noremap = true }
+    for k, v in pairs(extra) do opts[k] = v end
+    map(mode, binding, command, opts)
+  end
+end
+local nmap = modemap('n')
+local imap = modemap('i')
+local vmap = modemap('v')
 
 -- map leader to space
-map('n', '<space>', '', {})
+nmap('<space>', '')
 vim.g.mapleader = ' '
 
 -- pane switching
-map('n', '<leader>h', ':wincmd h<cr>', opts)
-map('n', '<leader>j', ':wincmd j<cr>', opts)
-map('n', '<leader>k', ':wincmd k<cr>', opts)
-map('n', '<leader>l', ':wincmd l<cr>', opts)
+nmap('<leader>h', ':wincmd h<cr>')
+nmap('<leader>j', ':wincmd j<cr>')
+nmap('<leader>k', ':wincmd k<cr>')
+nmap('<leader>l', ':wincmd l<cr>')
 
 -- clipboard operations
 -- Copy to clipboard
-map('v', '<leader>y', '"+y', opts)
-map('n', '<leader>y', '"+y', opts)
-map('n', '<leader>yg_', '"+yg_', opts)
-map('n', '<leader>yy', '"+yy', opts)
+vmap('<leader>y', '"+y')
+nmap('<leader>y', '"+y')
+nmap('<leader>yg_', '"+yg_')
+nmap('<leader>yy', '"+yy')
 
 -- Paste from clipboard
-map('v', '<leader>p', '"+p', opts)
-map('v', '<leader>P', '"+P', opts)
-map('n', '<leader>p', '"+p', opts)
-map('n', '<leader>P', '"+P', opts)
+vmap('<leader>p', '"+p')
+vmap('<leader>P', '"+P')
+nmap('<leader>p', '"+p')
+nmap('<leader>P', '"+P')
+
+-- make 'Y' like 'C' and 'D'
+nmap('Y', 'yg_')
+
+-- keep center cursor
+nmap('n', 'nzz')
+nmap('N', 'Nzz')
+
+-- add punctuation to undo break points
+imap(',', ',<c-g>u')
+imap('.', '.<c-g>u')
+imap('!', '!<c-g>u')
+imap('?', '?<c-g>u')
+imap('[', '[<c-g>u')
+
+-- add relative moves to jumplist
+nmap('k', '(v:count > 5 ? "m\'" . v:count : "") . "k"', { expr = true })
+nmap('j', '(v:count > 5 ? "m\'" . v:count : "") . "j"', { expr = true })
