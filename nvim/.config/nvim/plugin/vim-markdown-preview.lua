12a5e1848bbc7ddc28984fff8bf6a93982911919
Sean Ye <yesean@amazon.com>
2021-08-31 11:41:45 -0700

    Update markdown

--- nvim/.config/nvim/plugin/vim-markdown-preview.lua
+++ nvim/.config/nvim/plugin/vim-markdown-preview.lua
@@ -1,4 +1,4 @@
-vim.g.vim_markdown_preview_toggle = 2
+vim.g.vim_markdown_preview_toggle = 1
 vim.g.vim_markdown_preview_hotkey = '<leader>m'
-vim.g.vim_markdown_preview_browser = 'Safari'
+vim.g.vim_markdown_preview_browser = 'Firefox'
 vim.g.vim_markdown_preview_github = 1
