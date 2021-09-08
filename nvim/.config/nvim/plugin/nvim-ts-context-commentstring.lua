9cab405a6f1a2a338c00e52f648281c9bdc230ad
Sean Ye <seanye24@gmail.com>
2021-09-08 04:57:19 -0700

    Update kommentary ts hack

---
+++ nvim/.config/nvim/plugin/nvim-ts-context-commentstring.lua
@@ -0,0 +1,13 @@
+require('nvim-treesitter.configs').setup({
+  context_commentstring = {
+    enable = true,
+    enable_autocmd = false,
+  },
+})
+require('kommentary.config').configure_language('typescriptreact', {
+  single_line_comment_string = 'auto',
+  multi_line_comment_strings = 'auto',
+  hook_function = function()
+    require('ts_context_commentstring.internal').update_commentstring()
+  end,
+})
