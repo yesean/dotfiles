c922af6621f866e6429adc3f6d699cb8a8773481
Sean Ye <yesean@amazon.com>
2021-09-10 15:16:23 -0700

    Update telescope find_files

--- nvim/.config/nvim/plugin/telescope.lua
+++ nvim/.config/nvim/plugin/telescope.lua
@@ -6,7 +6,7 @@ local grep_command = {
   '--with-filename',
   '--line-number',
   '--column',
-  '--smart-case'
+  '--smart-case',
 }
 
 require('telescope').setup({ defaults = { vimgrep_arguments = grep_command } })
@@ -16,14 +16,31 @@ local map = vim.api.nvim_set_keymap
 local opts = { noremap = true }
 
 local find_command =
-    [[{ 'fd', '--hidden', '--follow', '--exclude', '{.git,node_modules}' }]]
+  [[{ 'fd', '--type', 'f', '--hidden', '--follow', '--exclude', '{.git,node_modules}' }]]
 
-map('n', '<c-p>',
-    [[<cmd>lua require('telescope.builtin').find_files({ find_command = ]] ..
-        find_command .. [[})<cr>]], opts)
-map('n', '<c-f>', [[<cmd>lua require('telescope.builtin').live_grep({})<cr>]],
-    opts)
-map('n', '<leader>fb', '<cmd>lua require(\'telescope.builtin\').buffers()<cr>',
-    opts)
-map('n', '<leader>fh',
-    '<cmd>lua require(\'telescope.builtin\').help_tags()<cr>', opts)
+map(
+  'n',
+  '<c-p>',
+  [[<cmd>lua require('telescope.builtin').find_files({ find_command = ]]
+    .. find_command
+    .. [[})<cr>]],
+  opts
+)
+map(
+  'n',
+  '<c-f>',
+  [[<cmd>lua require('telescope.builtin').live_grep({})<cr>]],
+  opts
+)
+map(
+  'n',
+  '<leader>fb',
+  "<cmd>lua require('telescope.builtin').buffers()<cr>",
+  opts
+)
+map(
+  'n',
+  '<leader>fh',
+  "<cmd>lua require('telescope.builtin').help_tags()<cr>",
+  opts
+)
