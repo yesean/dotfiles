a13a6e4971885d6d2a3caf84e7df06e737961ee1
Sean Ye <seanye24@gmail.com>
2021-09-09 18:49:10 -0700

    Update ts keymaps

--- nvim/.config/nvim/plugin/lspconfig.lua
+++ nvim/.config/nvim/plugin/lspconfig.lua
@@ -85,7 +85,7 @@ local function on_attach_ts(client, bufnr)
   local opts = { silent = true }
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gs', ':TSLspOrganize<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', ':TSLspRenameFile<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', ':TSLspImportAll<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gI', ':TSLspImportAll<CR>', opts)
 end
 
 -- setup lsp clients
