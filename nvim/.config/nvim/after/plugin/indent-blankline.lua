764ae5de4e793458bbdc0e8186c0fc23d8b5ff6c
Sean Ye <yesean@amazon.com>
2021-09-10 18:11:10 -0700

    Update blankline

---
+++ nvim/.config/nvim/after/plugin/indent-blankline.lua
@@ -0,0 +1,23 @@
+vim.opt.termguicolors = true
+vim.opt.listchars = { lead = '.', eol = '↴' }
+vim.opt.list = true
+vim.g.indent_blankline_buftype_exclude = { 'terminal' }
+
+vim.cmd([[highlight IndentBlanklineIndent1 guifg=#E06C75 blend=nocombine]])
+vim.cmd([[highlight IndentBlanklineIndent2 guifg=#E5C07B blend=nocombine]])
+vim.cmd([[highlight IndentBlanklineIndent3 guifg=#98C379 blend=nocombine]])
+vim.cmd([[highlight IndentBlanklineIndent4 guifg=#56B6C2 blend=nocombine]])
+vim.cmd([[highlight IndentBlanklineIndent5 guifg=#61AFEF blend=nocombine]])
+vim.cmd([[highlight IndentBlanklineIndent6 guifg=#C678DD blend=nocombine]])
+
+require('indent_blankline').setup({
+  char_highlight_list = {
+    'IndentBlanklineIndent1',
+    'IndentBlanklineIndent2',
+    'IndentBlanklineIndent3',
+    'IndentBlanklineIndent4',
+    'IndentBlanklineIndent5',
+    'IndentBlanklineIndent6',
+  },
+  buftype_exclude = { 'terminal' },
+})
