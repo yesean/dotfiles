1b138ec1109758cf862daae97841152d6a7230ac
Sean Ye <seanye24@gmail.com>
2021-09-14 02:26:13 -0700

    Refactor indent-blankline config

--- nvim/.config/nvim/after/plugin/indent-blankline.lua
+++ nvim/.config/nvim/after/plugin/indent-blankline.lua
@@ -1,14 +1,13 @@
 vim.opt.termguicolors = true
-vim.opt.listchars = { lead = '.', eol = '↴' }
-vim.opt.list = true
-vim.g.indent_blankline_buftype_exclude = { 'terminal' }
-
 vim.cmd([[highlight IndentBlanklineIndent1 guifg=#E06C75 blend=nocombine]])
 vim.cmd([[highlight IndentBlanklineIndent2 guifg=#E5C07B blend=nocombine]])
 vim.cmd([[highlight IndentBlanklineIndent3 guifg=#98C379 blend=nocombine]])
 vim.cmd([[highlight IndentBlanklineIndent4 guifg=#56B6C2 blend=nocombine]])
 vim.cmd([[highlight IndentBlanklineIndent5 guifg=#61AFEF blend=nocombine]])
 vim.cmd([[highlight IndentBlanklineIndent6 guifg=#C678DD blend=nocombine]])
+
+vim.opt.listchars = { lead = '⋅', eol = '↴' }
+vim.opt.list = true
 
 require('indent_blankline').setup({
   char_highlight_list = {
