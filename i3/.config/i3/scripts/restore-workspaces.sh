485cdce3c89413eafee99aca80fdd37722ca852c
Sean Ye <seanye24@gmail.com>
2021-09-20 15:55:08 -0700

    Update i3

--- i3/.config/i3/scripts/restore-workspaces.sh
+++ i3/.config/i3/scripts/restore-workspaces.sh
@@ -12,10 +12,10 @@ i3-resurrect restore -w 8
 i3-resurrect restore -w 9
 i3-resurrect restore -w 10
 
-if [[ $(rg --hidden --pcre2 '\"class\": \"\^Google\\\\-chrome\$\"' ~/.i3/i3-resurrect/workspace*) != "" ]]; then
-  google-chrome-stable --restore-last-session
-fi
-
 if [[ $(rg --hidden --pcre2 '\"class\": \"\^Brave\\\\-browser\$\"' ~/.i3/i3-resurrect/workspace*) != "" ]]; then
   brave
+fi
+
+if [[ $(rg --hidden --pcre2 '\"class\":\"\^Caprine\$\"' ~/.i3/i3-resurrect/workspace*) != "" ]]; then
+  caprine
 fi
