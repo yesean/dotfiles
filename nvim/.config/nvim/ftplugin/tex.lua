45f336f09bcb063a8ca9f17c14dd47d54c5d1989
Sean Ye <seanye24@gmail.com>
2021-09-30 15:24:25 -0700

    Update latex save behavior

--- nvim/.config/nvim/ftplugin/tex.lua
+++ nvim/.config/nvim/ftplugin/tex.lua
@@ -1,19 +1,4 @@
-local map = vim.api.nvim_set_keymap
-local modemap = function(mode)
-  return function(binding, command, extra)
-    extra = extra or {}
-    local opts = { noremap = true }
-    for k, v in pairs(extra) do
-      opts[k] = v
-    end
-    map(mode, binding, command, opts)
-  end
-end
-local nmap = modemap('n')
-local imap = modemap('i')
-local vmap = modemap('v')
-
-nmap('<leader>c', ':silent !pdflatex %<cr>')
+vim.cmd([[autocmd BufWritePost * silent !pdflatex %]])
 
 -- set default ft as tex
 local g = vim.g
