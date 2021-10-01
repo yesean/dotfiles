81af44c7a460d70914b1ad95f27a7b51fef48d9e
Sean Ye <seanye24@gmail.com>
2021-10-01 14:08:46 -0700

    Add clipboard functionality to rofi calculator

--- rofi/.config/rofi/scripts/launch-calculator.sh
+++ rofi/.config/rofi/scripts/launch-calculator.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rofi -show calc -modi calc -no-show-match -no-sort -theme ~/.dotfiles/rofi/.config/rofi/themes/calculator.rasi
+rofi -show calc -modi calc -no-show-match -no-sort -theme ~/.dotfiles/rofi/.config/rofi/themes/calculator.rasi -calc-command "echo -n '{result}' | xclip -selection clipboard"
