d345acbff449590c7ccbc40360ca173effa516de
Sean Ye <seanye24@gmail.com>
2021-10-16 15:43:48 -0700

    Update plugins

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -2,46 +2,58 @@ require('packer').startup(function()
   -- package manager
   use('wbthomason/packer.nvim')
 
-  -- neovim requirements
+  -- essentials
   use('nvim-lua/popup.nvim')
   use('nvim-lua/plenary.nvim')
   use('kyazdani42/nvim-web-devicons')
 
-  -- core neovim plugins
+  -- syntax
   use({ 'nvim-treesitter/nvim-treesitter', run = 'TSUpdate' })
+
+  -- lsp
   use('neovim/nvim-lspconfig')
   use('williamboman/nvim-lsp-installer')
+
+  -- completion
   use('hrsh7th/nvim-cmp')
   use('hrsh7th/cmp-nvim-lsp')
+  use('saadparwaiz1/cmp_luasnip')
+  use('hrsh7th/cmp-path')
   use('hrsh7th/cmp-buffer')
-  use('quangnguyen30192/cmp-nvim-ultisnips')
+
+  -- file search
   use('nvim-telescope/telescope.nvim')
   use({ 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' })
 
+  -- snippets
+  use('L3MON4D3/LuaSnip')
+  use('rafamadriz/friendly-snippets')
+
   -- misc neovim plugins
-  use('folke/trouble.nvim')
   use('jose-elias-alvarez/null-ls.nvim')
   use('jose-elias-alvarez/nvim-lsp-ts-utils')
+
+  -- visuals
+  use('navarasu/onedark.nvim')
+  use('folke/trouble.nvim')
   use('romgrk/barbar.nvim')
   use('kyazdani42/nvim-tree.lua')
   use('hoob3rt/lualine.nvim')
   use('lewis6991/gitsigns.nvim')
-  use('windwp/nvim-autopairs')
-  use('windwp/nvim-ts-autotag')
-  use('navarasu/onedark.nvim')
-  use({ 'npxbr/glow.nvim', branch = 'main' })
-  use('b3nj5m1n/kommentary')
-  use('JoosepAlviste/nvim-ts-context-commentstring')
   use('onsails/lspkind-nvim')
   use('kosayoda/nvim-lightbulb')
-  use('blackCauldron7/surround.nvim')
   use('lukas-reineke/indent-blankline.nvim')
-  use('numtostr/FTerm.nvim')
   use('haringsrob/nvim_context_vt')
   use('goolord/alpha-nvim')
 
-  -- vim plugins
-  use('SirVer/ultisnips')
-  use('mlaursen/vim-react-snippets')
+  -- editing
+  use('windwp/nvim-autopairs')
+  use('windwp/nvim-ts-autotag')
+  use('b3nj5m1n/kommentary')
+  use('JoosepAlviste/nvim-ts-context-commentstring')
+  use('blackCauldron7/surround.nvim')
+
+  -- markdown
+  use({ 'npxbr/glow.nvim', branch = 'main' })
   use('JamshedVesuna/vim-markdown-preview')
 end)
--- nvim/.config/nvim/plugin/fterm.lua
+++
@@ -1,4 +0,0 @@
-local map = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
-map('n', '<leader>;', '<CMD>lua require("FTerm").toggle()<CR>', opts)
-map('t', '<leader>;', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>', opts)
