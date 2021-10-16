2179ee93629fd697dda0ca9d429bcd588d1a13d9
Sean Ye <seanye24@gmail.com>
2021-10-16 15:54:34 -0700

    Update snippets

--- nvim/.config/nvim/plugin/luasnip.lua
+++ nvim/.config/nvim/plugin/luasnip.lua
@@ -1,4 +1,2 @@
 -- load friendly-snippets
-require('luasnip/loaders/from_vscode').lazy_load({
-  paths = { '~/.local/share/nvim/site/pack/packer/start/friendly-snippets/' },
-})
+require('luasnip/loaders/from_vscode').lazy_load()
