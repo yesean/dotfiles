00118bf9a6f356cf462f0e6d14408d7be337b642
Sean Ye <seanye24@gmail.com>
2022-01-12 17:02:04 -0800

    Update polybar

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,14 +1,33 @@
 #!/usr/bin/env bash
 
-# Add this script to your wm startup file.
-
+## Files and Directories
 DIR="$HOME/.config/polybar"
+SFILE="$DIR/system"
 
-# Terminate already running bar instances
-killall -q polybar
+## Get system variable values for various modules
+get_values() {
+  INTERFACE=$(ip link | awk '/state UP/ {print $2}' | tr -d :)
+}
 
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+## Write values to `system` file
+set_values() {
+  if [[ "$INTERFACE" ]]; then
+    sed -i -e "s/network_interface = .*/network_interface = $INTERFACE/g" "$SFILE"
+  fi
+}
 
-# Launch the bar
-polybar -q main -c "$DIR"/config.ini &
+## Launch Polybar with selected style
+launch_bar() {
+  if [[ ! $(pidof polybar) ]]; then
+    polybar -q main -c "$DIR"/config &
+  else
+    polybar-msg cmd restart
+  fi
+
+  if type "xrandr" >/dev/null; then
+    echo "Lanching polybar for each screen"
+    xrandr --listactivemonitors | grep -oP '(HDMI\-\d+$|eDP\-\d+$)' | xargs -P1 -I{} bash -c "MONITOR={} polybar -q -r p00 &"
+  fi
+}
+
+launch_bar
