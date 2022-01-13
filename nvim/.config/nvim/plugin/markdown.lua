8081ee9532aa9da1f14ccbd7d15784502bdafda5
Sean Ye <seanye24@gmail.com>
2022-01-12 16:45:33 -0800

    Update markdown settings

--- nvim/.config/nvim/plugin/markdown.lua
+++ nvim/.config/nvim/plugin/markdown.lua
@@ -1,5 +1,5 @@
 vim.g.vim_markdown_preview_toggle = 1
 vim.g.vim_markdown_preview_hotkey = '<leader>m'
-vim.g.vim_markdown_preview_browser = 'Brave'
+vim.g.vim_markdown_preview_browser = 'Brave Browser'
 vim.g.vim_markdown_preview_use_xdg_open = 1
 vim.g.vim_markdown_preview_github = 1
