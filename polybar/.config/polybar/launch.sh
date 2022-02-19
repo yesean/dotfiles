b58b5de4963871c731ef6fb2ec51df50d5ff991b
Sean Ye <seanye24@gmail.com>
2022-02-18 23:45:46 -0800

    Refactor polybar

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,24 +1,28 @@
 #!/usr/bin/env bash
 
-## Files and Directories
+# files and directories
 DIR="$HOME/.config/polybar"
 SFILE="$DIR/system"
 
-## Get system variable values for various modules
+# get system variable values for various modules
 get_values() {
   INTERFACE=$(ip link | awk '/state UP/ {print $2}' | tr -d :)
 }
 
-## Write values to `system` file
+# write values to `system` file
 set_values() {
-  if [[ "$INTERFACE" ]]; then
-    sed -i -e "s/network_interface = .*/network_interface = $INTERFACE/g" "$SFILE"
+  if [ "$INTERFACE" ]; then
+    echo "Setting INTERFACE to $INTERFACE"
+    sed -i -e "s/network_interface =.*/network_interface = $INTERFACE/g" "$SFILE"
   fi
 }
 
-## Launch Polybar with selected style
+# launch polybar with selected style
 launch_bar() {
-  if [[ ! $(pidof polybar) ]]; then
+  get_values
+  set_values
+
+  if [ ! "$(pidof polybar)" ]; then
     polybar -q main -c "$DIR"/config &
   else
     polybar-msg cmd restart
