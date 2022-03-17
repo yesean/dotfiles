855796f54ad65ee64b6bda52c93e652b59154762
Sean Ye <seanye24@gmail.com>
2022-03-17 15:14:36 -0700

    switch to navigator mappings

--- nvim/.config/nvim/plugin/lsp.lua
+++ nvim/.config/nvim/plugin/lsp.lua
@@ -2,25 +2,6 @@ local util = require('lspconfig/util')
 local lsp_installer = require('nvim-lsp-installer')
 local maps = require('maps')
 
-local add_default_maps = function(bfr)
-  local lc = function(cmd, prefix)
-    prefix = prefix or 'lua'
-    return '<cmd>' .. prefix .. ' ' .. cmd .. '<cr>'
-  end
-  maps.bn(bfr, 'gt', lc('vim.lsp.buf.type_definition()'))
-  maps.bn(bfr, 'gD', lc('vim.lsp.buf.declaration()'))
-  maps.bn(bfr, 'gi', lc('vim.lsp.buf.implementation()'))
-  maps.bn(bfr, 'K', lc('vim.lsp.buf.hover()'))
-  maps.bn(bfr, '<C-k>', lc('vim.lsp.buf.signature_help()'))
-  maps.bn(bfr, '<space>rn', lc('vim.lsp.buf.rename()'))
-  maps.bn(bfr, '<space>a', lc('vim.lsp.buf.code_action()'))
-  maps.bn(bfr, '<space>e', lc('vim.diagnostic.open_float()'))
-  maps.bn(bfr, '[d', lc('vim.diagnostic.goto_prev()'))
-  maps.bn(bfr, ']d', lc('vim.diagnostic.goto_next()'))
-  maps.bn(bfr, '<space>ql', lc('vim.diagnostic.set_loclist()'))
-  maps.bn(bfr, '<space>f', lc('vim.lsp.buf.formatting()'))
-end
-
 local turn_off_formatting = function(client)
   client.resolved_capabilities.document_formatting = false
   client.resolved_capabilities.document_range_formatting = false
@@ -33,7 +14,6 @@ local capabilities = require('cmp_nvim_lsp').update_capabilities(
 
 -- define keymaps when lsp client attaches
 local on_attach_default = function(client, bfr)
-  add_default_maps(bfr)
   turn_off_formatting(client)
   require('navigator.lspclient.mapping').setup({
     client = client,
@@ -106,14 +86,67 @@ lsp_installer.on_server_ready(function(server)
   vim.cmd('do User LspAttachBuffers')
 end)
 
+local build_keymaps = function()
+  local keymaps = {
+    { 'gr', "require('navigator.reference').async_ref()" },
+    { '<m-k>', 'signature_help()', mode = 'i' },
+    { '<c-k>', 'signature_help()' },
+    { 'g0', "require('navigator.symbols').document_symbols()" },
+    { 'gW', "require('navigator.workspace').workspace_symbol()" },
+    { '<c-]>', "require('navigator.definition').definition()" },
+    { 'gd', "require('navigator.definition').definition()" },
+    { 'gD', "declaration({ border = 'rounded', max_width = 80 })" },
+    { 'gt', 'type_definition()' },
+    { 'gp', "require('navigator.definition').definition_preview()" },
+    { 'K', 'hover({ popup_opts = { border = single, max_width = 80 }})' },
+    {
+      '<leader>a',
+      "require('navigator.codeAction').code_action()",
+      mode = 'n',
+    },
+    { '<leader>A', 'range_code_action()', mode = 'v' },
+    { '<leader>rn', "require('navigator.rename').rename()" },
+    { 'gi', 'implementation()' },
+    { '<leader>e', "require('navigator.diagnostics').show_diagnostics()" },
+    { 'gG', "require('navigator.diagnostics').show_buf_diagnostics()" },
+    { 'gGG', "require('navigator.diagnostics').toggle_diagnostics()" },
+    { ']d', "diagnostic.goto_next({ border = 'rounded', max_width = 80})" },
+    { '[d', "diagnostic.goto_prev({ border = 'rounded', max_width = 80})" },
+    { ']O', 'diagnostic.set_loclist()' },
+    { ']r', "require('navigator.treesitter').goto_next_usage()" },
+    { '[r', "require('navigator.treesitter').goto_previous_usage()" },
+    { '<leader>k', "require('navigator.dochighlight').hi_symbol()" },
+    { '<leader>wa', "require('navigator.workspace').add_workspace_folder()" },
+    {
+      '<leader>wr',
+      "require('navigator.workspace').remove_workspace_folder()",
+    },
+    { '<Space>ff', 'formatting()', mode = 'n' },
+    { '<Space>ff', 'range_formatting()', mode = 'v' },
+    {
+      '<Space>wl',
+      "require('navigator.workspace').list_workspace_folders()",
+    },
+    { '<Space>la', "require('navigator.codelens').run_action()", mode = 'n' },
+  }
+  local navigator_keymaps = {}
+  for _, v in ipairs(keymaps) do
+    local keymap = { key = v[1], func = v[2] }
+    if v.mode then
+      keymap.mode = v.mode
+    end
+    table.insert(navigator_keymaps, keymap)
+  end
+  return navigator_keymaps
+end
+
 vim.diag = vim.diagnostic
 require('navigator').setup({
   lsp_installer = true,
   lsp = {
     format_on_save = false,
+    disable_format_cap = {},
   },
-  keymaps = {
-    { key = '[d', func = 'vim.diag.goto_prev()' },
-    { key = ']d', func = 'vim.diag.goto_next()' },
-  },
+  default_mapping = false,
+  keymaps = build_keymaps(),
 })
