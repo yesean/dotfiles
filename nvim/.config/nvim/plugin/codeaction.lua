1e9e427462ecf68c0363f2d48c05791b4bfec87c
Sean Ye <seanye24@gmail.com>
2022-03-17 15:15:01 -0700

    remove nvim lightbulb

--- nvim/.config/nvim/plugin/codeaction.lua
+++
@@ -1,6 +0,0 @@
--- check if lightbulb is available at cursor position
-vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-  callback = function()
-    require('nvim-lightbulb').update_lightbulb()
-  end,
-})
