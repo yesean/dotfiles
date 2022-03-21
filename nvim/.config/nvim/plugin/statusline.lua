5b78faaeee3059ab8cfc27cd1d625f273a752308
Sean Ye <seanye24@gmail.com>
2022-03-21 16:49:21 -0700

    change lualine theme, use relative paths

--- nvim/.config/nvim/plugin/statusline.lua
+++ nvim/.config/nvim/plugin/statusline.lua
@@ -1,9 +1,9 @@
 require('lualine').setup({
-  options = { theme = 'gruvbox-flat' },
+  options = { theme = 'catppuccin' },
   sections = {
     lualine_a = { 'mode' },
     lualine_b = { 'branch' },
-    lualine_c = { 'filename' },
+    lualine_c = { { 'filename', path = 1, shorting_target = 40 } },
     lualine_x = { 'encoding' },
     lualine_y = { 'location', 'progress' },
     lualine_z = { 'filetype' },
