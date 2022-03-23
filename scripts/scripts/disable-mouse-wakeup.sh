8371a142ab2d0b486f4a49f733cc7c4a48aa11b4
Sean Ye <seanye24@gmail.com>
2022-03-23 15:11:53 -0700

    disable mouse wakeup on startup

---
+++ scripts/scripts/disable-mouse-wakeup.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+product_file=$(grep -l "USB Receiver" /sys/bus/usb/devices/*/product)
+wakeup_file=${product_file/product/power\/wakeup}
+tee "$wakeup_file" <<<disabled >/dev/null
