df5aefc3ca442d728d3485834aa2a0829ee55027
Sean Ye <seanye24@gmail.com>
2022-04-10 21:36:06 -0700

    remove comments

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -44,9 +44,7 @@ require('packer').startup(function()
   use({ 'catppuccin/nvim', as = 'catppuccin' })
 
   -- visuals
-  -- use('romgrk/barbar.nvim')
   use('noib3/nvim-cokeline')
-  -- use('kyazdani42/nvim-tree.lua')
   use({ 'nvim-neo-tree/neo-tree.nvim', branch = 'v2.x' })
   use('hoob3rt/lualine.nvim')
   use('lewis6991/gitsigns.nvim')
