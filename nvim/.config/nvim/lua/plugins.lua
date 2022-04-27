5b616dc64400550732e3c1dc00eb186e161dd43e
Sean Ye <seanye24@gmail.com>
2022-04-26 19:14:37 -0700

    add git blame mapping

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -117,6 +117,8 @@ require('packer').startup({
       'f-person/git-blame.nvim',
       config = function()
         vim.g.gitblame_enabled = false
+        local map = require('mapping')
+        map.n('<leader>gb', map.cmd('GitBlameToggle'))
       end,
     })
     use({
