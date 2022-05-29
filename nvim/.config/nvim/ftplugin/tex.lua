9feece5a078ce8e097f488f377dd2937f5a7fd82
Sean Ye <seanye24@gmail.com>
2022-05-29 00:11:14 -0700

    update tex cmd

--- nvim/.config/nvim/ftplugin/tex.lua
+++ nvim/.config/nvim/ftplugin/tex.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_create_augroup('LatexCompile', {})
 vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
-  command = 'AsyncRun pdflatex -shell-escape %; rm -rf %:r.{aux,log,out} indent.log _minted-%:t:r', -- compile pdf, delete misc files
+  command = 'AsyncRun pdflatex -shell-escape %; rm -rf %:t:r.{aux,log,out} indent.log _minted-%:t:r', -- compile pdf, delete misc files
   group = 'LatexCompile',
 })
