8e9a366e6a43e62c960d82ad5fc45cd3c986b225
Sean Ye <seanye24@gmail.com>
2022-06-09 15:57:39 -0700

    update neotree

--- nvim/.config/nvim/lua/config/neotree.lua
+++ nvim/.config/nvim/lua/config/neotree.lua
@@ -9,6 +9,7 @@ require('neo-tree').setup({
   filesystem = {
     filtered_items = {
       hide_dotfiles = false,
+      hide_gitignored = false,
     },
     follow_current_file = true,
     hijack_netrw_behavior = 'open_current',
