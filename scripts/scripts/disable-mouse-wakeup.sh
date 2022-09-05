51e061893be34bac7306b4d09dea5e31b0ba13b7
Sean Ye <seanye24@gmail.com>
2022-09-04 22:41:29 -0700

    script fixes

--- scripts/scripts/disable-mouse-wakeup.sh
+++ scripts/scripts/disable-mouse-wakeup.sh
@@ -2,8 +2,12 @@
 
 product_file=$(grep -l "USB Receiver" /sys/bus/usb/devices/*/product)
 wakeup_file=${product_file/product/power\/wakeup}
-tee "$wakeup_file" <<<disabled >/dev/null
+if [ -f "$wakeup_file" ]; then
+  tee "$wakeup_file" <<<disabled >/dev/null
+fi
 
 product_file=$(grep -l "PRO X Wireless" /sys/bus/usb/devices/*/product)
 wakeup_file=${product_file/product/power\/wakeup}
-tee "$wakeup_file" <<<disabled >/dev/null
+if [ -f "$wakeup_file" ]; then
+  tee "$wakeup_file" <<<disabled >/dev/null
+fi
