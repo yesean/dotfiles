5f7344c74ce179bfb30716d644722511814e46ae
Sean Ye <seanye24@gmail.com>
2022-10-24 15:19:25 -0700

    Handle no active windows

--- scripts/scripts/window.sh
+++ scripts/scripts/window.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
 
-winid=$(xdotool getactivewindow)
+winid=$(xdotool getactivewindow 2>/dev/null)
+if [ "$winid" = "" ]; then
+  echo arch && exit 0
+fi
+
 winclass=$(xprop -id "$winid" | grep "WM_CLASS(STRING)" | cut -d "=" -f 2 | cut -d "," -f 2 | tr -d '"')
 echo "$winclass"
