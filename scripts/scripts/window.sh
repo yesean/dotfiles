537acf16649257fc4692df3b62fbf69ae269fb7d
Sean Ye <seanye24@gmail.com>
2022-10-25 17:47:08 -0700

    Bold application name, improve script

--- scripts/scripts/window.sh
+++ scripts/scripts/window.sh
@@ -5,5 +5,5 @@ if [ "$winid" = "" ]; then
   echo arch && exit 0
 fi
 
-winclass=$(xprop -id "$winid" | grep "WM_CLASS(STRING)" | cut -d "=" -f 2 | cut -d "," -f 2 | tr -d '"')
+winclass=$(xprop -id "$winid" | grep "WM_CLASS(STRING)" | grep -v "grep" | cut -d "=" -f 2 | cut -d "," -f 2 | tr -d '"' | xargs)
 echo "$winclass"
