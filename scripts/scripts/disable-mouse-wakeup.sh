07caae0fcc45dc0aeed1e4d458c1b1137abaf8b9
Sean Ye <seanye24@gmail.com>
2022-11-29 00:37:15 -0800

    Refactor script

--- scripts/scripts/disable-mouse-wakeup.sh
+++ scripts/scripts/disable-mouse-wakeup.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-product_file=$(grep -l "USB Receiver" /sys/bus/usb/devices/*/product)
-wakeup_file=${product_file/product/power\/wakeup}
-if [ -f "$wakeup_file" ]; then
-  tee "$wakeup_file" <<<disabled >/dev/null
-fi
+superlight_mouse_receiver="USB Receiver"
+superlight_mouse_wired="USB Receiver"
 
-product_file=$(grep -l "PRO X Wireless" /sys/bus/usb/devices/*/product)
-wakeup_file=${product_file/product/power\/wakeup}
-if [ -f "$wakeup_file" ]; then
-  tee "$wakeup_file" <<<disabled >/dev/null
-fi
+disable() {
+  product_file=$(grep -l "$1" /sys/bus/usb/devices/*/product)
+  wakeup_file=${product_file/product/power\/wakeup}
+  if [ -f "$wakeup_file" ]; then
+    tee "$wakeup_file" <<<disabled >/dev/null
+  fi
+}
+
+disable "$superlight_mouse_receiver"
+disable "$superlight_mouse_wired"
