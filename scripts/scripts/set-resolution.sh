b720f5e9a9df35b03ad5db9eddd3ec2c90ac6395
Sean Ye <seanye24@gmail.com>
2022-12-01 15:27:32 -0800

    Use dynamic display adapter

--- scripts/scripts/set-resolution.sh
+++ scripts/scripts/set-resolution.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env sh
 
+display_adapter=$(xrandr | grep " connected " | cut -f 1 -d " ")
+
 if [ "$1" = 'full' ]; then
-  xrandr --output DP-0 --mode 3440x1440 --rate 100
+  xrandr --output "$display_adapter" --mode 3440x1440 --rate 100
   i3-msg restart
 elif [ "$1" = 'half' ]; then
-  xrandr --output DP-0 --mode 1720x1440 --rate 100
+  xrandr --output "$display_adapter" --mode 1720x1440 --rate 100
   i3-msg restart
 else
   echo 'set-resolution <full|half>'
