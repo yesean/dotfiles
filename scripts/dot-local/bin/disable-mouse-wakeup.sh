578f0bfa0745f5a9a733119cb28089c09f148c2f
Sean Ye <seanye24@gmail.com>
2023-04-01 14:17:06 -0700

    Refactor

--- scripts/dot-local/bin/disable-mouse-wakeup.sh
+++ scripts/dot-local/bin/disable-mouse-wakeup.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 
-superlight_mouse_receiver="USB Receiver"
-superlight_mouse_wired="USB Receiver"
-
 disable() {
   grep -l "$1" /sys/bus/usb/devices/*/product |
     sed -e 's/product/power\/wakeup/g' |
     xargs -d '\n' -I{} sh -c '[ -f {} ] && tee {} <<<disabled >/dev/null'
 }
 
-disable "$superlight_mouse_receiver"
-disable "$superlight_mouse_wired"
+disable "USB Receiver" # Logitech G Pro X Superlight
