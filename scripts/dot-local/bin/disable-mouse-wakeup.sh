e5dcbbbfc2e5741d966790fc7f650b2b33d7ffeb
Sean Ye <seanye24@gmail.com>
2023-04-01 14:14:37 -0700

    Handle multiple matches

--- scripts/dot-local/bin/disable-mouse-wakeup.sh
+++ scripts/dot-local/bin/disable-mouse-wakeup.sh
@@ -4,11 +4,9 @@ superlight_mouse_receiver="USB Receiver"
 superlight_mouse_wired="USB Receiver"
 
 disable() {
-  product_file=$(grep -l "$1" /sys/bus/usb/devices/*/product)
-  wakeup_file=${product_file/product/power\/wakeup}
-  if [ -f "$wakeup_file" ]; then
-    tee "$wakeup_file" <<<disabled >/dev/null
-  fi
+  grep -l "$1" /sys/bus/usb/devices/*/product |
+    sed -e 's/product/power\/wakeup/g' |
+    xargs -d '\n' -I{} sh -c '[ -f {} ] && tee {} <<<disabled >/dev/null'
 }
 
 disable "$superlight_mouse_receiver"
