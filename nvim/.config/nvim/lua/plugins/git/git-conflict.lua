df5c4faa1ae8c7fbd09b95b30027af8348d7a956
Sean Ye <seanye24@gmail.com>
2023-04-20 13:36:30 -0700

    Update git-conflict setup

--- nvim/.config/nvim/lua/plugins/git/git-conflict.lua
+++ nvim/.config/nvim/lua/plugins/git/git-conflict.lua
@@ -2,11 +2,13 @@ return {
   {
     'akinsho/git-conflict.nvim',
     version = '*',
+    event = { 'BufRead' },
     opts = {
       default_mappings = {
         prev = '[x',
         next = ']x',
       },
     },
+    config = true,
   },
 }
