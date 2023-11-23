8411d822b7b60b26ff984bbeb394df6b2991c7c5
Sean Ye <seanye24@gmail.com>
2023-11-23 13:10:03 -0500

    Update wezterm theme

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -5,7 +5,7 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
-config.color_scheme = 'Catppuccin Macchiato'
+config.color_scheme = 'Tokyo Night'
 config.font_size = 10.5
 
 config.use_fancy_tab_bar = false
