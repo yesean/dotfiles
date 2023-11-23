ab11ec0f9eca4a92e606a9fd253a4533b2123121
Sean Ye <seanye24@gmail.com>
2023-11-23 12:26:07 -0500

    Configure luacheck

--- nvim/.config/nvim/lua/plugins/lint.lua
+++ nvim/.config/nvim/lua/plugins/lint.lua
@@ -11,11 +11,19 @@ return {
         markdown = { 'markdownlint', 'vale' },
         ['*'] = { 'write_good' },
       },
-      linters = { luacheck = { args = { '--globals', 'vim' } } },
+      linters = {
+        luacheck = { args = { '--globals', 'vim' } },
+      },
     },
     config = function(_, opts)
-      require('lint').linters_by_ft = opts.linters_by_ft
-      require('lint').linters = opts.linters
+      local lint = require('lint')
+
+      -- override default linter configs
+      lint.linters_by_ft = opts.linters_by_ft
+      for linter, config in pairs(opts.linters) do
+        lint.linters[linter].args = config.args
+      end
+
       vim.api.nvim_create_autocmd(
         { 'BufWritePost', 'BufReadPost', 'InsertLeave' },
         {
