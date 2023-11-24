f4c309efc9b99855c6c26127c2fc6a53ff4b6c94
Sean Ye <seanye24@gmail.com>
2023-11-24 17:56:09 -0500

    Add session saving autocmd

--- nvim/.config/nvim/lua/plugins/session.lua
+++ nvim/.config/nvim/lua/plugins/session.lua
@@ -2,6 +2,17 @@ return {
   {
     'folke/persistence.nvim',
     event = 'BufReadPre',
-    config = true,
+    config = function(_, opts)
+      local persistence = require('persistence')
+
+      persistence.setup(opts)
+      vim.api.nvim_create_autocmd('BufEnter', {
+        desc = 'save neovim session whenever a buffer is opened',
+        group = 'persistence',
+        callback = function()
+          persistence.save()
+        end,
+      })
+    end,
   },
 }
