7eceb4e4f87d7268433b26467ad58b9ad5f830d8
Sean Ye <seanye24@gmail.com>
2023-12-01 23:15:17 -0500

    Add case insensitive completion in zsh

--- zsh/.zsh/completion.zsh
+++ zsh/.zsh/completion.zsh
@@ -1,7 +1,9 @@
 # completion settings
 autoload -Uz compinit && compinit
 
-zstyle ':completion:*' ignored-patterns '*.|*..' # ignore the special dirs . and .. in tab completion
+zstyle ':completion:*' ignored-patterns '*.|*..'        # ignore the special dirs . and .. in tab completion
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'  # case insentive matching https://superuser.com/questions/1092033/how-can-i-make-zsh-tab-completion-fix-capitalization-errors-for-directories-and
+
 export ZSH_AUTOSUGGEST_STRATEGY=(history completion)
 export ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
 export ZSH_AUTOSUGGEST_USE_ASYNC=1
