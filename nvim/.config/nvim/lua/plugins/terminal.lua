08cbc48a1312869e7edcd0e931602a2bfb577f52
Sean Ye <seanye24@gmail.com>
2024-02-11 12:29:31 -0500

    Change toggleterm.nvim binding

--- nvim/.config/nvim/lua/plugins/terminal.lua
+++ nvim/.config/nvim/lua/plugins/terminal.lua
@@ -1,5 +1,11 @@
 return {
   { 'skywind3000/asyncrun.vim' }, -- for compiling latex, reference: ftplugin/tex.lua
   { 'lambdalisue/suda.vim' },
-  { 'akinsho/toggleterm.nvim', opts = { size = 35, open_mapping = '<c-`>' } },
+  {
+    'akinsho/toggleterm.nvim',
+    opts = {
+      size = 35,
+      open_mapping = '<c-j>',
+    },
+  },
 }
