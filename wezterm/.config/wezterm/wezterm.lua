5ea3e1e67c8f203f968b1246043f15a2587e50be
Sean Ye <seanye24@gmail.com>
2024-02-12 23:30:58 -0500

    Update wezterm config
    * remove config_build null check
    * turn checking for updates

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,15 +1,13 @@
 local wezterm = require('wezterm')
 
-local config = {}
-if wezterm.config_builder then
-  config = wezterm.config_builder()
-end
+local config = wezterm.config_builder()
 
 config.color_scheme = 'Tokyo Night'
 config.font_size = 10.5
 
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
+config.check_for_updates = false
 
 config.keys = {
   {
