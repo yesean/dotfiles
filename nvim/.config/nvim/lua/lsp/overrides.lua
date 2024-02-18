73add899c9eebb3686f13f4d5648030c3b519378
Sean Ye <seanye24@gmail.com>
2024-02-18 02:01:42 -0500

    Add eslint autofix on save

--- nvim/.config/nvim/lua/lsp/overrides.lua
+++ nvim/.config/nvim/lua/lsp/overrides.lua
@@ -10,6 +10,14 @@ return {
       },
     },
   },
+  eslint = {
+    on_attach = function(_, bufnr)
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        buffer = bufnr,
+        command = 'EslintFixAll',
+      })
+    end,
+  },
   lua_ls = {
     settings = {
       Lua = {
