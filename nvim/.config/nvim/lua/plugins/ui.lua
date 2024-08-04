50c0de134889f6c33420e2af4127bdbeb2ef2e4a
Sean Ye <seanye24@gmail.com>
2024-08-04 18:16:32 -0400

    [nvim] Disable plugin to silence deprecation warning

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -151,6 +151,7 @@ return {
   },
   {
     'kosayoda/nvim-lightbulb',
+    enabled = false,
     opts = { autocmd = { enabled = true } },
     config = function(_, opts)
       require('nvim-lightbulb').setup(opts)
