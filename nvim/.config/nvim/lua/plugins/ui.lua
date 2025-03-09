8665e5b28733c6b1836f62622177385e9d2c12ef
Sean Ye <seanye24@gmail.com>
2025-03-09 17:27:01 -0400

    disable hover notifications

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -265,6 +265,7 @@ return {
           ['vim.lsp.util.stylize_markdown'] = true,
           ['cmp.entry.get_documentation'] = true,
         },
+        hover = { silent = true },
       },
       -- you can enable a preset for easier configuration
       presets = {
