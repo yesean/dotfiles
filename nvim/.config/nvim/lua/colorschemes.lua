136f54991d3caf307dcd80feba1c4ee5462aaacf
Sean Ye <seanye24@gmail.com>
2025-03-09 17:14:08 -0400

    add colorschemes

--- nvim/.config/nvim/lua/colorschemes.lua
+++ nvim/.config/nvim/lua/colorschemes.lua
@@ -19,9 +19,28 @@ local colorschemes = {
       vim.cmd.colorscheme('tokyonight-night')
     end,
   },
+  everforest = {
+    'neanias/everforest-nvim',
+    version = false,
+    lazy = false,
+    priority = 1000, -- make sure to load this before all the other start plugins
+    -- Optional; default configuration will be used if setup isn't called.
+    config = function()
+      require('everforest').load()
+    end,
+  },
+  ['rose-pine'] = {
+    'rose-pine/neovim',
+    name = 'rose-pine',
+    opts = { styles = { italic = false } },
+    config = function(_, opts)
+      require('rose-pine').setup(opts)
+      vim.cmd('colorscheme rose-pine')
+    end,
+  },
 }
 
-local selected_colorscheme_name = 'tokyonight'
+local selected_colorscheme_name = 'rose-pine'
 
 return {
   selected_colorscheme_name = selected_colorscheme_name,
