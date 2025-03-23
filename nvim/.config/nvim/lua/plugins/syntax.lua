811ff897c633d9b898d7bd0b03ed3b1cfc6eb095
Sean Ye <seanye24@gmail.com>
2025-03-23 02:25:19 -0400

    remove treesitter manual reset function

--- nvim/.config/nvim/lua/plugins/syntax.lua
+++ nvim/.config/nvim/lua/plugins/syntax.lua
@@ -23,33 +23,11 @@ return {
       vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
       vim.o.foldenable = false
 
-      local function resetHighlighting()
-        local buffer = table.concat(vim.api.nvim_buf_get_lines(0, 0, -1, false))
-        local is_buffer_empty = buffer == ''
-        if
-          vim.bo.buftype == ''
-          and not vim.bo.readonly
-          and not is_buffer_empty
-          and not utils.is_vscode
-        then
-          vim.cmd.w()
-          vim.cmd.e()
-          vim.cmd.TSBufEnable('highlight')
-        end
-      end
-
-      -- highlighting fix, restart treesitter when opening a new file
-      vim.api.nvim_create_autocmd(
-        { 'VimEnter' },
-        { callback = resetHighlighting }
-      )
-
       -- always use jsonc parser
       vim.api.nvim_create_autocmd({ 'BufEnter' }, {
         pattern = '*.json',
         callback = function()
           vim.bo.filetype = 'jsonc'
-          resetHighlighting()
         end,
       })
 
