fe3702c58aa13eea29f115ba1835fbfdebee47e2
Sean Ye <seanye24@gmail.com>
2025-03-23 02:21:23 -0400

    reenable nvim-lightbulb

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -174,7 +174,6 @@ return {
   },
   {
     'kosayoda/nvim-lightbulb',
-    enabled = false,
     opts = { autocmd = { enabled = true } },
     config = function(_, opts)
       require('nvim-lightbulb').setup(opts)
